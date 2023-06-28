83a03d59137ad3ef2a7e2c649e334823a2e68db2
yu hamba <yuhamba@yunoMacBook-Air-155781.local>
2023-06-28 20:20:36 +0900

    色変更を追加しました

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 $("#addToDo").click(function() {
     const inputToDo = $("input").val();
     $("#todoList").append("<li><input type='checkbox'>" + inputToDo + "</li>");
-
     $("input").val("");
 });
 
 $(document).on("change", "input[type=checkbox]", function() {
     if($(this).is(":checked")) {
         $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "#ccc");
     } else {
         $(this).parent().css("text-decoration", "none"); 
+        $(this).parent().css("color", "#000");
     }
 })
